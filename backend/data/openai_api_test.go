e5c4076278eccf7ab8720b28be77901c110c2b21
ArvinLovegood <arvinlovegood@163.com>
2025-03-19 15:54:51 +0800

    test:注释掉测试函数中的数据库初始化代码
    - 在 openai_api_test.go 和 stock_data_api_test.go 文件中
    - 注释掉了 TestNewDeepSeekOpenAiConfig 和 TestSearchGuShiTongStockInfo 函数中的 db.Init 调用
    - 这可能是为了在不依赖数据库的情况下进行测试，提高测试的独立性和可维护性

--- backend/data/openai_api_test.go
+++ backend/data/openai_api_test.go
@@ -2,12 +2,11 @@ package data
 
 import (
 	"context"
-	"go-stock/backend/db"
 	"testing"
 )
 
 func TestNewDeepSeekOpenAiConfig(t *testing.T) {
-	db.Init("../../data/stock.db")
+	//db.Init("../../data/stock.db")
 	ai := NewDeepSeekOpenAi(context.TODO())
 	res := ai.NewChatStream("上海贝岭", "sh600171", "分析以上股票资金流入信息，找出适合买入的股票，给出具体操作建议")
 	for {
@@ -23,7 +22,7 @@ func TestGetTopNewsList(t *testing.T) {
 }
 
 func TestSearchGuShiTongStockInfo(t *testing.T) {
-	db.Init("../../data/stock.db")
+	//db.Init("../../data/stock.db")
 	SearchGuShiTongStockInfo("hk01810", 60)
 	SearchGuShiTongStockInfo("sh600745", 60)
 	SearchGuShiTongStockInfo("gb_goog", 60)
